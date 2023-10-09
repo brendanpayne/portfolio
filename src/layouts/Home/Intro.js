e7178d24bb8a121d342e177265a29e1c76098044
brendanpayne <payneb2@mail.uc.edu>
2023-10-09 14:22:31 -0400

    worked on intro some more

--- src/layouts/Home/Intro.js
+++ src/layouts/Home/Intro.js
@@ -18,13 +18,13 @@ export function Intro({ id, sectionRef, scrollIndicatorHidden, ...rest}) {
 
         >
             <Transition in timeout={3000}>
-                {(isPresent, status) => (
+                {(isVisible, status) => (
                 <Fragment>
                     <header className={styles.text}>
-                        <h1 className={styles.title} data-present={isPresent} id={titleId}>
+                        <h1 className={styles.name} data-visible={isVisible} id={titleId}>
                             Brendan Payne
                         </h1>
-                        <p className={styles.description} data-present={isPresent}>
+                        <p className={styles.title} data-visible={isVisible}>
                             Software Engineer
                         </p>
                     </header>
